9a4c877779220daff5bb4daf348b526964cf0b19
Ricardo Alfredo Macias Olvera <ra_mo116@hotmail.com>
2019-05-04 19:03:30 -0700

    simple leaky membrane - Adding input current

---
+++ script2.m
@@ -0,0 +1,37 @@
+#/1 A simple leaky membrane
+
+clear
+% membrane constants
+tau_m = 0.030;
+Rm = 3e7;
+
+% Time
+delta_t = 0.001; % Integration time step
+duration = 0.5; % duration of the simulation
+No_steps = round(duration ./ delta_t);
+times = linspace(0, duration, No_steps + 1); % The time points for update
+
+% membrane potential
+V_0 = 0.05; % The initial membrane potential
+V = zeros(1, No_steps + 1);
+V(1) = V_0;
+
+% Injection current
+I_0 = 2e-9;
+I_duration = 0.25;
+I_start = 0.05;
+I_start_index = round(I_start ./ delta_t);
+I_finish_index = round((I_start + I_duration) ./ delta_t);
+Iinj = zeros(1, No_steps + 1);
+Iinj(I_start_index:I_finish_index) = I_0;
+
+
+for i=1:No_steps
+  #dV = (-1 ./ tau_m) .* V(i) .* delta_t;
+  dV = (1 ./ tau_m) .* (-V(i) + Rm .* Iinj(i)) .* delta_t;
+   V(i+1) = V(i) + dV;
+end
+
+clf % clear the graphics
+plot(times, V);
+
